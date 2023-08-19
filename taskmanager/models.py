995d9e4ddeb5b907c5cc2cc3937a0d1e9680fc2e
Melhefnawi <>
2023-08-19 14:43:56 +0000

    all

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,29 +1,30 @@
 from taskmanager import db
 
-class Category(db.Model):
 
-    id = db.column(db.Integer, primary_key=True)
-    category_name = db.column(db.String(25), unique=True, nullable=False)
+class Category(db.Model):
+    # schema for the Category model
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(25), unique=True, nullable=False)
     tasks = db.relationship("Task", backref="category",
-     cascade="all,delete",lazy=True)
+                            cascade="all, delete", lazy=True)
 
     def __repr__(self):
-        #__repr__ to represent it self inform of a string
-
+        # __repr__ to represent itself in the form of a string
         return self.category_name
 
-class Task(db.Model):
 
-    id = db.column(db.Integer, primary_key=True)
-    task_name = db.column(db.String(50), unique = True, nullable = False)
-    task_description = db.column(db.Text, nullable=False)
-    is_urgent= db.column(db.Boolean, default = False, nullable=False)
-    due_date = DB.column(db.Date, nullable=Fale)
-    category_id= db.column(db.Integer, db.foreignkey("category.id"),
-     ondelete="CASCADE", nullable=False)
+class Task(db.Model):
+    # schema for the Task model
+    id = db.Column(db.Integer, primary_key=True)
+    task_name = db.Column(db.String(50), unique=True, nullable=False)
+    task_description = db.Column(db.Text, nullable=False)
+    is_urgent = db.Column(db.Boolean, default=False, nullable=False)
+    due_date = db.Column(db.Date, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey(
+        "category.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
-       # __repr__ to represent it self in a form of a string
-
-       return "{0}- Task:{1} | urgent :{2}".format(self.id, self.task_name,
-        self.is_urgent)
+        # __repr__ to represent itself in the form of a string
+        return "#{0} - Task: {1} | Urgent: {2}".format(
+            self.id, self.task_name, self.is_urgent
+        )
